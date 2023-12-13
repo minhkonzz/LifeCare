010cfdb9aa6f309ab56592b492b534bf4d9ece6b
minhkonzz <minhphm37@gmail.com>
2023-12-13 13:51:11 +0700

    refactor code phase 4

--- src/hocs/withPopupBehavior.tsx
+++ src/hocs/withPopupBehavior.tsx
@@ -2,9 +2,11 @@ import { memo, ComponentType, Dispatch, SetStateAction, useRef } from 'react'
 import { Animated } from 'react-native'
 import Popup from '@components/shared/popup'
 
-export default <P extends object>(MainPopup: ComponentType<P>) => {
+export default <P extends object>(MainPopup: ComponentType<P>, isConfirm: boolean) => {
    return memo(({ setVisible }: { setVisible: Dispatch<SetStateAction<any>> }) => {
       const animateValue: Animated.Value = useRef<Animated.Value>(new Animated.Value(0)).current
+
+      
       
       return (
          <Popup {...{
